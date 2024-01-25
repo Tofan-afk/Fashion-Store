9710e9f40a8cce3adfe0c6d6873dfeb7f7417b4a
Tofan-afk <datspy300@gmail.com>
2024-01-25 17:44:07 +0200

    about and shop
    pretty much done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,76 @@
-export default function About() {}
+import React, { useState, useEffect } from "react";
+import "../SASS/about.scss";
+import {
+  Services,
+  Video,
+  Reviews,
+  Recent_Blogs,
+  Instagram_Post,
+  Present,
+} from "./Components";
+
+export default function About() {
+  return (
+    <div id="about">
+      <Present title="About Us" src="./about-background.png" />
+      <StartedSec />
+      <Services />
+      <Video />
+      <Reviews />
+      <Recent_Blogs />
+      <Stores />
+      <Instagram_Post />
+    </div>
+  );
+}
+
+function StartedSec() {
+  return (
+    <div classNam="wide_container" id="startedsec_container">
+      <h2>How was swanky started?</h2>
+      <p>
+        Risus augue curabitur diam senectus congue velit et. Sed vitae metus
+        nibh sit a tristique. Nulla adipiscing pharetra pellentesque maecenas
+        odio eros at. Et libero vulputate habitasse amet duis erat volutpat
+        vitae eget. Quam libero etiam et in ac at quis. Sed id parturient
+        viverra blandit. Ultrices congue id odio ullamcorper tellus et.
+        Vulputate venenatis malesuada nulla neque neque sollicitudin eu. Lorem
+        ipsum dolor sit amet consectetur. Eget sollicitudin magna egestas massa
+        accumsan orci leo. Nisl odio in proin ultrices mi et mattis vel nulla.
+        Mattis auctor id ut eget augue sed cras. Eget tortor cursus dignissim
+        purus penatibus. At placerat dis dis tincidunt nunc nisl vivamus vitae
+        felis.{" "}
+      </p>
+    </div>
+  );
+}
+
+function Stores() {
+  return (
+    <div id="our_stores_container">
+      <img />
+      <div id="stores_mini_container">
+        <h2>Our stores</h2>
+        <p>You can also directly buy products from our stores.</p>
+        <div>
+          <h3>USA</h3>
+          <p>730 Glenstone Ave 65802, Springfield, US</p>
+          <p>
+            +123 222 333 44 <br />
+            +123 666 777 88
+          </p>
+          <p>swanky@yourinfo.com</p>
+        </div>
+        <div>
+          <h3>France</h3>
+          <p>13 Rue Montmartre 75001, Paris, France</p>
+          <p>
+            +123 222 333 44 <br />
+            +123 666 777 88
+          </p>
+          <p>swanky@yourinfo.com</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,1 +1,104 @@
-export default function Shop() {}
+import React, { useState, useEffect } from "react";
+import "../SASS/shop.scss";
+import {
+  Reviews,
+  Recent_Blogs,
+  Instagram_Post,
+  Present,
+  Logo_Buttons,
+} from "./Components";
+
+export default function Shop() {
+  return (
+    <div id="shop">
+      <Present title="Shop" src="./shop-background.png" />
+      <Shop_Window />
+      <Reviews />
+      <Recent_Blogs />
+      <Logo_Buttons />
+      <Instagram_Post />
+    </div>
+  );
+}
+
+function Shop_Window() {
+  const shop_img = [
+    { src: "./shop/car.png", alt: "car" },
+    { src: "./shop/distress_beanie.png", alt: "beanin" },
+    { src: "./shop/fancy.png", alt: "fancy" },
+    { src: "./shop/red_dress.png", alt: "r_dress" },
+    { src: "./shop/outside.png", alt: "out" },
+    { src: "./shop/something.png", alt: "smth" },
+    { src: "./shop/greer.png", alt: "green" },
+    { src: "./shop/black_dress.png", alt: "b_dress" },
+    { src: "./shop/couch.png", alt: "couch" },
+  ];
+  return (
+    <div id="shop_window_container">
+      <div id="shop_showcase">
+        {shop_img.map((e) => (
+          <div>
+            <img src={e.src} alt={e.alt} />
+            <h2>{e.alt}</h2>
+            <p>$7.00</p>
+          </div>
+        ))}
+      </div>
+      <div id="categories">
+        <form>
+          <input placeholder="Enter your email address..."></input>
+          <button>
+            <svg
+              width="22"
+              height="21"
+              viewBox="0 0 22 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19.6234 17.2836L15.5805 13.4244C16.5538 12.1875 17.0793 10.682 17.0775 9.135C17.0775 5.18355 13.7096 1.96875 9.57 1.96875C5.43039 1.96875 2.0625 5.18355 2.0625 9.135C2.0625 13.0864 5.43039 16.3013 9.57 16.3013C11.1906 16.3029 12.7679 15.8014 14.0637 14.8723L18.1066 18.7314C18.3113 18.9061 18.5782 18.9993 18.8526 18.9919C19.127 18.9846 19.388 18.8773 19.5821 18.692C19.7762 18.5067 19.8886 18.2576 19.8963 17.9956C19.904 17.7337 19.8063 17.4789 19.6234 17.2836ZM4.2075 9.135C4.2075 8.12261 4.52201 7.13295 5.11124 6.29118C5.70048 5.4494 6.53799 4.79332 7.51786 4.40589C8.49773 4.01847 9.57595 3.9171 10.6162 4.11461C11.6564 4.31211 12.6119 4.79963 13.3619 5.5155C14.1118 6.23137 14.6225 7.14344 14.8295 8.13638C15.0364 9.12932 14.9302 10.1585 14.5243 11.0939C14.1184 12.0292 13.4311 12.8286 12.5492 13.3911C11.6674 13.9535 10.6306 14.2537 9.57 14.2537C8.1483 14.2521 6.78532 13.7123 5.78002 12.7527C4.77473 11.7931 4.20921 10.4921 4.2075 9.135Z"
+                fill="#040404"
+              />
+            </svg>
+          </button>
+        </form>
+        <h2>Categories</h2>
+        <p>All</p>
+        <p>Popular</p>
+        <p>Accesories</p>
+        <p>New Arrivals</p>
+        <p>Trendings</p>
+
+        <h2>Tags</h2>
+        <p>Classic</p>
+        <p>Tees</p>
+        <p>Leather Coat</p>
+        <p>Winter</p>
+        <p>Summer</p>
+        <p>Modern</p>
+        <p>Sleevless</p>
+
+        <h2>Filter by Color</h2>
+        <p>Red</p>
+        <p>Orange</p>
+        <p>Green</p>
+        <p>Blue</p>
+        <p>Black</p>
+
+        <h2>Filter by size</h2>
+        <p>XL</p>
+        <p>L</p>
+        <p>M</p>
+        <p>S</p>
+        <p>XS</p>
+
+        <h2>Filter by price</h2>
+        <p>Less than $10</p>
+        <p>$10- $20</p>
+        <p>$20- $30</p>
+        <p>$30- $40</p>
+        <p>$40- $50</p>
+      </div>
+    </div>
+  );
+}
